7767aa0d0ca621147e57fa50bc391760c0ee65b7
石井康介 <faitayahhuu@yahoo.co.jp>
2019-09-02 23:16:18 +0900

    usertokenのバグ修正

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\GoogleUser;
 
 class HomeController extends Controller
 {
@@ -27,18 +28,19 @@ class HomeController extends Controller
         if (! Auth::check()) {
             return view('home-login-error');
         }
-        $user = Auth::user();
+
+        $authuser = Auth::user();
+        $user = GoogleUser::find($authuser['id']);
         $date = time();
 
-        if($user['token_exp_time'] + 3000 <= $date) { #期限切れ１０分前から更新可能
-            $token = auth('api')->login($user);
+        if($user['token_exp_time'] - 600 <= $date) { #期限切れ１０分前から更新可能
+            $token = auth('api')->login($authuser);
             $user['token'] = $token;
             $user['token_exp_time'] = $date + 3600;
             $user->save();
         } else {
             $token = $user['token'];
         }
-        $exp = $user['token_exp_time'];
-        return view('home', compact('user','token','exp'));
+        return view('home', compact('user','token'));
     }
 }
